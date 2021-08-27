66b39ce406c28b1257bec2bf1c836066fe8ce9dd
Levi Meahan <lsmjudoka@gmail.com>
2021-08-27 00:05:26 -0700

    fix vars for new server

--- variables.php
+++ variables.php
@@ -6,10 +6,15 @@ Created:	02/21/2012
 Revised:	04/15/2014
 Purpose:	Store global variables such as master link, settings for script to use
 */
-$ENVIRONMENT = 'dev';
+
+$ENVIRONMENT = 'prod';
+
 // Update for different website
 $dev_link = "http://lsmjudoka.com/projects/shinobi_chronicles/";
-$prod_link = "http://shinobi-chronicles.com/";
+// $prod_link = "http://shinobi-chronicles.com/";
+$prod_link = "https://gracious-easley.162-243-171-116.plesk.page/";
+
+/** @var string $link root link to webpage */
 if($ENVIRONMENT == 'dev') {
 	$link = $dev_link;
 }
@@ -19,9 +24,11 @@ else {
 
 // Master on/off switch
 $SC_OPEN = true;
+
 // Training boost switches
 $TRAIN_BOOST = 0; // Extra points per training, 0 for none
 $LONG_TRAIN_BOOST = 0; // Extra points per long training, 0 for none
+
 // Links for pages to be linked to from other pages
 $members_link = $link . '?id=6';
 $mod_link = $link . '?id=16';
@@ -30,6 +37,7 @@ $report_link = $link . '?id=18';
 $battle_link = $link . '?id=19';
 $spar_link = $link . '?id=22';
 $mission_link = $link . '?id=23';
+
 // Staff levels
 $SC_MODERATOR = 1;
 $SC_HEAD_MODERATOR = 2;
@@ -60,12 +68,14 @@ $SC_STAFF_COLORS = array(
 		'pm_class' => 'administrator'
 	)
 );
+
 // Default layout
 $DEFAULT_LAYOUT = 'shadow_ribbon';
 $VERSION_NUMBER = '0.8.0';
+
 // Map size
 $MAP_SIZE_X = 18;
 $MAP_SIZE_Y = 12;
+
 // Misc stuff
 $SC_MAX_RANK = 4;
-?>
