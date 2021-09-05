13ae27a07843a4a99d553e84daa446177676b26b
Levi Meahan <lsmjudoka@gmail.com>
2021-09-04 20:57:24 -0700

    Remove vars.php

--- secure/vars.php
+++
@@ -1,5 +0,0 @@
-<?php
-$host = 'localhost';
-$username = 'schost_gameuser';
-$password = 'fGom4_46';
-$database = 'schost_game';
---
+++ secure/vars.sample.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Make a copy of this as vars.php and change values to the ones for your environment */
+$host = 'localhost';
+$username = 'database_username';
+$password = 'database_password';
+$database = 'database_name';
