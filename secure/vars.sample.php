db703d7a9318e27abbbc6320de0a444be10be537
Levi Meahan <lsmjudoka@gmail.com>
2021-09-04 20:58:13 -0700

    Tweak comment

--- secure/vars.sample.php
+++ secure/vars.sample.php
@@ -1,6 +1,9 @@
 <?php
 
-/* Make a copy of this as vars.php and change values to the ones for your environment */
+/*
+Make a copy of this as vars.php and change values to the ones for your environment. Then
+manually upload to your server, outside of git.
+ */
 $host = 'localhost';
 $username = 'database_username';
 $password = 'database_password';
