c3e13b6ae691f40511486fb9a548517ec22918a9
Levi Meahan <lsmjudoka@gmail.com>
2021-09-06 21:08:20 -0700

    Add contrib notes to manual

--- manual.php
+++ manual.php
@@ -24,6 +24,7 @@ echo str_replace("[HEADER_TITLE]", "Rules", $body_start);
 
 <table class='table'><tr><th>Manual</th></tr>
     <tr><td>
+        <b>Contributing</b>
         To submit changes to this manual (you may need a GitHub account):
         <ol>
             <li>Go to <a href='https://github.com/levimeahan/shinobi-chronicles/blob/main/manual.php'>manual.php on GitHub</a></li>
@@ -32,6 +33,11 @@ echo str_replace("[HEADER_TITLE]", "Rules", $body_start);
             <li>Scroll down and click "Propose Changes"</li>
             <li>Click "Create Pull Request"</li>
         </ol>
+
+        <!-- Content -->
+        <div>
+
+        </div>
     </td></tr>
 </table>
 
