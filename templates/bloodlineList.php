7f1e885620b11e40f673d4cfd69e0b69a245c12d
HitoriWoB <anubiswork@gmail.com>
2021-10-13 08:08:39 -0700

    Changed text case

--- templates/bloodlineList.php
+++ templates/bloodlineList.php
@@ -32,21 +32,21 @@
     <tr><td>
         <p>Using the form below, you can search for bloodlines based on rank.</p>
         <div style="text-align:center;">
-            <?php FOREACH($ranks as $id=>$name): ?>
+            <?php foreach($ranks as $id=>$name): ?>
                 <input type="checkbox" onclick="toggleBloodlineDetails('<?=$id?>_rank')"
                        class="<?=$id?>_rank_box" checked="checked" /><?=$name?>
-            <?php ENDFOREACH ?>
+            <?php endforeach ?>
         </div>
     </td></tr>
 </table>
 
 <table class="table">
-    <?php FOREACH($ranks as $id=>$rank): ?>
-        <?php IF(empty($bloodlines[$id])): ?>
+    <?php foreach($ranks as $id=>$rank): ?>
+        <?php if(empty($bloodlines[$id])): ?>
             <?php continue; ?>
-        <?php ELSE: ?>
+        <?php else: ?>
             <tr class="<?=$id?>_rank"><th><?=$rank?></th></tr>
-            <?php FOREACH($bloodlines[$id] as $bloodline_id => $bloodline): ?>
+            <?php foreach($bloodlines[$id] as $bloodline_id => $bloodline): ?>
                 <tr class="<?=$id?>_rank"><td>
                     <a onclick="toggleBloodlineDetails('<?=$bloodline_id?>_details')"
                        style="cursor:pointer;"><?=$bloodline['name']?></a><br />
@@ -56,46 +56,46 @@
                         <?php if(isset($bloodline['passive_boosts']) && !empty($bloodline['passive_boosts'])): ?>
                             <label style="width:9.5em; font-weight:bold;">Passive Boosts:</label><br />
                             <div style="margin-left:1.5em;">
-                            <?php FOREACH($bloodline['passive_boosts'] as $passive_boost): ?>
+                            <?php foreach($bloodline['passive_boosts'] as $passive_boost): ?>
                                 <?=ucwords(str_replace('_', ' ', $passive_boost->effect))?> =>
                                 <?= $passive_boost->power ?><br />
-                            <?php ENDFOREACH ?>
+                            <?php endforeach ?>
                             </div>
-                        <?php ENDIF ?>
+                        <?php endif ?>
                         <!-- Combat Boost List -->
                         <?php if(isset($bloodline['combat_boosts']) && !empty($bloodline['combat_boosts'])): ?>
                             <label style="width:9.5em; font-weight:bold;">Combat Boosts:</label><br />
                             <div style="margin-left:1.5em;">
-                                <?php FOREACH($bloodline['combat_boosts'] as $combat_boost): ?>
+                                <?php foreach($bloodline['combat_boosts'] as $combat_boost): ?>
                                     <?=ucwords(str_replace('_', ' ', $combat_boost->effect))?> =>
                                     <?= $combat_boost->power ?><br />
-                                <?php ENDFOREACH ?>
+                                <?php endforeach ?>
                             </div>
                         <?php ENDIF ?>
                         <!-- Jutsu List -->
                         <label style='width:4em; font-weight:bold;'>Jutsu:</label><br />
                         <div style="margin-left:1.5em;">
-                            <?php FOREACH($bloodline['jutsu'] as $ability): ?>
+                            <?php foreach($bloodline['jutsu'] as $ability): ?>
                                 <em><?=$ability->name?></em><br />
                                 <div style="margin-left:1.5em;">
                                     <label style="width:7.5em; font-weight:bold;">Rank:</label> <?=$RANK_NAMES[$ability->rank]?><br />
                                     <label style="width:7.5em; font-weight:bold;">Element:</label> <?=ucwords($ability->element)?><br />
                                     <label style="width:7.5em; font-weight:bold;">Use Cost:</label> <?=$ability->use_cost?><br />
                                     <label style="width:7.5em; font-weight:bold;">Effect:</label> <?=ucwords(str_replace('_', ' ', $ability->effect))?>
-                                        <?php IF($ability->effect != 'none'): ?>
+                                        <?php if($ability->effect != 'none'): ?>
                                             - <?=$ability->effect_length?> turn(s)<br />
-                                        <?php ELSE: ?>
+                                        <?php else: ?>
                                             <br />
-                                        <?php ENDIF ?>
+                                        <?php endif ?>
                                     <label style="width:7.5em; font-weight:bold;">Jutsu Type:</label> <?=ucwords($ability->jutsu_type)?><br />
                                     <label style="width:7.5em; font-weight:bold;">Description:</label> <?=$ability->description?><br />
                                     <br />
                                 </div>
-                            <?php ENDFOREACH ?>
+                            <?php endforeach ?>
                         </div>
                     </div>
                 </td></tr>
-            <?php ENDFOREACH ?>
-        <?php ENDIF ?>
-    <?php ENDFOREACH ?>
+            <?php endforeach ?>
+        <?php endif ?>
+    <?php endforeach ?>
 </table>
