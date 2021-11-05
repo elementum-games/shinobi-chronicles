e8c50bcc2bff4612930cf74330506a0658d5177b
Kelly Baillie <kbaillieworks@gmail.com>
2021-11-04 22:31:00 -0700

    Premium bug fix

--- chat.php
+++ chat.php
@@ -49,7 +49,7 @@ function chat() {
 			$title = $player->rank_name;
 			$staff_level = $player->staff_level;
 			$supported_colors = array(
-				'blue' => -1,
+				'black' => 0,'blue' => -1,
 				'pink' => -2,
 				'gold' => -3,
 				'green' => -4,
@@ -57,7 +57,45 @@ function chat() {
 				'purple' => -6,
 				'red' => -7,
 			);
-			$user_color = (!$player->forbidden_seal) ? 0 : ($supported_colors[$player->forbidden_seal['color']]);
+
+            // Restrict color if user is no longer able to use it
+            switch($player->chat_color) {
+                case 'blue':
+                case 'pink':
+                    if(!$player->forbidden_seal) {
+                        $player->chat_color = 'black';
+                    }
+                    break;
+                case 'gold':
+                    if(!$player->premium_credits_purchased) {
+                        $player->chat_color = 'black';
+                    }
+                    break;
+                case 'green':
+                    if(!$player->isModerator()) {
+                        $player->chat_color = 'black';
+                    }
+                    break;
+                case 'teal':
+                    if(!$player->isHeadAdmin()) {
+                        $player->chat_color = 'black';
+                    }
+                    break;
+                case 'purple':
+                    if(!$player->isContentAdmin() || !$player->isUserAdmin() || !$player->isHeadAdmin()) {
+                        $player->chat_color = 'black';
+                    }
+                    break;
+                case 'red':
+                    if(!$player->isUserAdmin() || !$player->isHeadAdmin()) {
+                        $player->chat_color = 'black';
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+			$user_color = (!$player->canChangeChatColor()) ? 0 : ($supported_colors[$player->chat_color]);
 
 			$sql = "INSERT INTO `chat`
                     (`user_name`, `message`, `title`, `village`, `staff_level`, `user_color`, `time`, `edited`) VALUES
