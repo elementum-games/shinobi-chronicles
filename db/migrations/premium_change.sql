1ae8b8285d46ac720704d0fec72ad5c1db4b7f48
Kelly Baillie <kbaillieworks@gmail.com>
2021-11-08 15:04:37 -0800

    Chat color array now controlled with user class

--- db/migrations/premium_change.sql
+++ db/migrations/premium_change.sql
@@ -1,1 +1,6 @@
+START TRANSACTION;
+
 ALTER TABLE `users` ADD `chat_color` VARCHAR(100) NOT NULL AFTER `forbidden_seal`;
+ALTER TABLE `chat` CHANGE `user_color` `user_color` VARCHAR(50) NOT NULL;
+
+COMMIT;
