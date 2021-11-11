e8db591a33697f4564807ae745937e5e5e45338a
Kelly Baillie <kbaillieworks@gmail.com>
2021-11-11 09:18:05 -0800

    Mod panel link error fix

--- modPanel.php
+++ modPanel.php
@@ -24,9 +24,9 @@ function modPanel() {
 	// Submenu
     echo "<div class='submenu'>
             <ul class='submenu'>
-                <li style='width:32.9%;'><a href='<?=$self_link?>'>Menu</a></li>
-                <li style='width:32.9%;'><a href='<?=$self_link?>&view=banned_users'>Banned Users</a></li>
-                <li style='width:32.9%;'><a href='<?=$self_link?>&view=locked_out_users'>Locked Out Users</a></li>
+                <li style='width:32.9%;'><a href='$self_link'>Menu</a></li>
+                <li style='width:32.9%;'><a href='$self_link&view=banned_users'>Banned Users</a></li>
+                <li style='width:32.9%;'><a href='$self_link&view=locked_out_users'>Locked Out Users</a></li>
             </ul>
         </div>
         <div class='submenuMargin'></div>";
