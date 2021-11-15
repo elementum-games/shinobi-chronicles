d36a8af56ac90cfa69bff209d68312b0ad062725
Levi Meahan <lsmjudoka@gmail.com>
2021-11-14 23:40:19 -0800

    Add FighterAction initialization

--- classes/battle/FighterAction.php
+++ classes/battle/FighterAction.php
@@ -15,4 +15,16 @@ class FighterAction {
         $this->jutsu_purchase_type = $jutsu_purchase_type;
         $this->weapon_id = $weapon_id;
     }
+
+    /**
+     * @param array $action_data
+     * @return FighterAction
+     */
+    public static function fromDb(array $action_data): FighterAction {
+        return new FighterAction(
+            $action_data['jutsu_id'],
+            $action_data['jutsu_purchase_type'],
+            $action_data['weapon_id']
+        );
+    }
 }
