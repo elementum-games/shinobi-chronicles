aeff1e3ee84f935624520e11403ea96ab65e77b7
Levi Meahan <lsmjudoka@gmail.com>
2022-01-14 00:19:26 -0800

    Fix kicking team creator

--- team.php
+++ team.php
@@ -292,7 +292,7 @@ function team() {
                         }
                     }
 
-                    if(!$kick_key) {
+                    if($kick_key === false) {
                         throw new Exception("Invalid user key!");
                     }
 
