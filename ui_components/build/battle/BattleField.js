059bdfa3b55c09dab804ec2d00bbd213a224e278
Levi Meahan <lsmjudoka@gmail.com>
2022-08-18 21:43:17 -0700

    Implement deterministic tile and fighter location rendering

--- ui_components/build/battle/BattleField.js
+++ ui_components/build/battle/BattleField.js
@@ -13,7 +13,6 @@ export default function BattleField({
   const containerRef = React.useRef(null);
 
   const setContainerRef = el => {
-    console.log(containerRef.current, el);
     containerRef.current = el;
   };
 
@@ -27,11 +26,16 @@ export default function BattleField({
       });
     }
   }, [containerRef.current]);
+  const tileSize = 80;
   return /*#__PURE__*/React.createElement("div", {
     className: `tilesContainer`,
+    style: {
+      height: tileSize
+    },
     ref: setContainerRef
   }, containerSize != null && /*#__PURE__*/React.createElement(BattleFieldTiles, {
     containerSize: containerSize,
+    tileSize: tileSize,
     tiles: tilesToDisplay,
     player: player,
     fighters: fighters,
@@ -44,6 +48,7 @@ export default function BattleField({
 
 function BattleFieldTiles({
   containerSize,
+  tileSize,
   tiles,
   player,
   fighters,
@@ -53,7 +58,6 @@ function BattleFieldTiles({
   onTileSelect
 }) {
   const [hoveredTile, setHoveredTile] = React.useState(null);
-  const tileSize = 70;
 
   const fightersForIds = ids => {
     return ids.map(id => fighters[id]).filter(Boolean);
@@ -99,8 +103,10 @@ function BattleFieldTiles({
     }
 
     return distanceToPlayer(tile.index) <= selectedJutsu.range && distanceToPlayer(hoveredTile) <= selectedJutsu.range && (hoveredTile > playerLocation && tile.index > playerLocation || hoveredTile < playerLocation && tile.index < playerLocation);
-  };
+  }; // Render magic
 
+
+  const fighterDisplaySize = 25;
   const freeWidth = containerSize.width - tileSize * tiles.length;
   const freeHeight = containerSize.height - tileSize;
 
@@ -112,22 +118,101 @@ function BattleFieldTiles({
     throw new Error("Container is not tall enough!");
   }
 
-  const leftPadding = freeWidth / (tiles.length + 1); // + 1 so we have an equal margin to the right of the last tile
+  const tileHorizontalGap = freeWidth / (tiles.length + 1); // + 1 so we have an equal margin to the right of the last tile
 
-  const topPadding = freeHeight / 2;
-  console.log("left padding", leftPadding);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, tiles.map((tile, i) => {
-    const cumulativeLeftPadding = leftPadding * (i + 1);
-    const cumulativeTileWidth = tileSize * i;
-    const tileStyles = {
+  const tileVerticalGap = freeHeight / 2;
+
+  function getBoundingRectForTile(tileIndex) {
+    /* Display index is based on which tiles are displayed, while tile index is the absolute index
+     if the fighters have moved to the right a few times, we might display tile indexes 4-9 like this:
+     4 5 6 7 8 9
+       But we need to do our calculations relative to what's visible, so tile index 4 here is tile display index 0
+    */
+    const tileDisplayIndex = tiles.findIndex(t => t.index === tileIndex);
+
+    if (tileDisplayIndex === -1) {
+      throw new Error(`Invalid tile! ${tileIndex}`);
+    }
+
+    const cumulativeLeftPadding = tileHorizontalGap * (tileDisplayIndex + 1);
+    const cumulativeTileWidth = tileSize * tileDisplayIndex;
+    return {
       width: tileSize,
       height: tileSize,
-      top: topPadding,
+      top: tileVerticalGap,
       left: cumulativeTileWidth + cumulativeLeftPadding
     };
+  }
+
+  const fighterIdsByTile = {};
+  Object.keys(fighterLocations).forEach(fighterId => {
+    const tileIndex = fighterLocations[fighterId];
+
+    if (fighterIdsByTile[tileIndex] == null) {
+      fighterIdsByTile[tileIndex] = [];
+    }
+
+    fighterIdsByTile[tileIndex].push(fighterId);
+  });
+
+  function distributeFightersOnTile(fightersOnTile) {
+    const allyFighters = fightersOnTile.filter(fighter => fighter.isAlly);
+    const enemyFighters = fightersOnTile.filter(fighter => !fighter.isAlly);
+    const fighterLocationsOnTile = {};
+    const spacingWhenTwoFighters = (tileSize - fighterDisplaySize * 2) / 3;
+    const spacingWhenOneFighter = (tileSize - fighterDisplaySize) / 2; // Two teams, spread horizontally
+
+    let allyLeftPosition, enemyLeftPosition;
+
+    if (allyFighters.length > 0 && enemyFighters.length > 0) {
+      allyLeftPosition = spacingWhenTwoFighters;
+      enemyLeftPosition = spacingWhenTwoFighters + fighterDisplaySize + spacingWhenTwoFighters;
+    } // One team, center horizontally
+    else {
+      allyLeftPosition = spacingWhenOneFighter;
+      enemyLeftPosition = spacingWhenOneFighter;
+    }
+
+    if (allyFighters.length === 2) {
+      fighterLocationsOnTile[allyFighters[0].id] = {
+        top: spacingWhenTwoFighters,
+        left: allyLeftPosition
+      };
+      fighterLocationsOnTile[allyFighters[1].id] = {
+        top: spacingWhenTwoFighters + fighterDisplaySize + spacingWhenTwoFighters,
+        left: allyLeftPosition
+      };
+    } else if (allyFighters.length === 1) {
+      fighterLocationsOnTile[allyFighters[0].id] = {
+        top: spacingWhenOneFighter,
+        left: allyLeftPosition
+      };
+    }
+
+    if (enemyFighters.length === 2) {
+      fighterLocationsOnTile[enemyFighters[0].id] = {
+        top: spacingWhenTwoFighters,
+        left: enemyLeftPosition
+      };
+      fighterLocationsOnTile[enemyFighters[1].id] = {
+        top: spacingWhenTwoFighters + fighterDisplaySize + spacingWhenTwoFighters,
+        left: enemyLeftPosition
+      };
+    } else if (enemyFighters.length === 1) {
+      fighterLocationsOnTile[enemyFighters[0].id] = {
+        top: spacingWhenOneFighter,
+        left: enemyLeftPosition
+      };
+    }
+
+    return fighterLocationsOnTile;
+  }
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, tiles.map(tile => {
+    const tileBoundingRect = getBoundingRectForTile(tile.index);
     return /*#__PURE__*/React.createElement("div", {
       className: "tileContainer",
-      style: tileStyles,
+      style: tileBoundingRect,
       key: tile.index
     }, /*#__PURE__*/React.createElement(BattleFieldTile, {
       index: tile.index,
@@ -141,12 +226,37 @@ function BattleFieldTiles({
       onMouseEnter: () => setHoveredTile(tile.index),
       onMouseLeave: () => setHoveredTile(null)
     }));
+  }), Object.keys(fighterIdsByTile).map(tileIndexStr => {
+    const tileIndex = parseInt(tileIndexStr);
+    const fighters = fightersForIds(fighterIdsByTile[tileIndex]);
+
+    if (fighters.length > 4) {
+      throw new Error(`Too many fighters on tile ${tileIndex}!`);
+    }
+
+    const tileBoundingRect = getBoundingRectForTile(tileIndex);
+    const fighterPositionsOnTile = distributeFightersOnTile(fighters);
+    return fighters.map(fighter => /*#__PURE__*/React.createElement("div", {
+      key: fighter.id,
+      className: `tileFighter ${fighter.isAlly ? 'ally' : 'enemy'}`,
+      style: {
+        top: fighterPositionsOnTile[fighter.id].top + tileBoundingRect.top,
+        left: fighterPositionsOnTile[fighter.id].left + tileBoundingRect.left,
+        width: fighterDisplaySize,
+        height: fighterDisplaySize
+      }
+    }, /*#__PURE__*/React.createElement(FighterAvatar, {
+      displaySize: fighterDisplaySize,
+      fighterName: fighter.name,
+      avatarLink: fighter.avatarLink,
+      maxAvatarSize: 20,
+      includeContainer: false
+    })));
   }));
 }
 
 function BattleFieldTile({
   index,
-  fighters,
   canMoveTo,
   showAttackTarget,
   showAttackPreview,
@@ -175,13 +285,5 @@ function BattleFieldTile({
     onMouseLeave: onMouseLeave
   }, /*#__PURE__*/React.createElement("span", {
     className: "tileIndex"
-  }, index), fighters.map((fighter, i) => /*#__PURE__*/React.createElement("div", {
-    key: i,
-    className: `tileFighter ${fighter.isAlly ? 'ally' : 'enemy'}`
-  }, /*#__PURE__*/React.createElement(FighterAvatar, {
-    fighterName: fighter.name,
-    avatarLink: fighter.avatarLink,
-    maxAvatarSize: 20,
-    includeContainer: false
-  }))));
+  }, index));
 }
--- ui_components/src/battle/BattleField.js
+++ ui_components/src/battle/BattleField.js
@@ -28,7 +28,6 @@ export default function BattleField({
 
     const containerRef = React.useRef(null);
     const setContainerRef = (el) => {
-        console.log(containerRef.current, el);
         containerRef.current = el;
     };
 
@@ -44,11 +43,14 @@ export default function BattleField({
         }
     }, [containerRef.current])
 
+    const tileSize = 80;
+
     return (
-        <div className={`tilesContainer`} ref={setContainerRef}>
+        <div className={`tilesContainer`} style={{ height: tileSize }} ref={setContainerRef}>
             {containerSize != null &&
                 <BattleFieldTiles
                     containerSize={containerSize}
+                    tileSize={tileSize}
                     tiles={tilesToDisplay}
                     player={player}
                     fighters={fighters}
@@ -68,6 +70,7 @@ type BattleFieldTilesProps = {|
         +width: number,
         +height: number,
     |},
+    +tileSize: number,
     +tiles: $ReadOnlyArray<BattleFieldTileType>,
     +player: FighterType,
     +fighters: { [ key: string ]: FighterType },
@@ -78,6 +81,7 @@ type BattleFieldTilesProps = {|
 |};
 function BattleFieldTiles({
     containerSize,
+    tileSize,
     tiles,
     player,
     fighters,
@@ -87,8 +91,6 @@ function BattleFieldTiles({
     onTileSelect
 }) {
     const [hoveredTile, setHoveredTile] = React.useState(null);
-
-    const tileSize = 70;
 
     const fightersForIds = (ids: $ReadOnlyArray<string>) => {
         return ids.map(id => fighters[ id ]).filter(Boolean)
@@ -144,6 +146,9 @@ function BattleFieldTiles({
             );
     };
 
+    // Render magic
+    const fighterDisplaySize = 25;
+
     const freeWidth = containerSize.width - (tileSize * tiles.length);
     const freeHeight = containerSize.height - tileSize;
     if(freeWidth < 0) {
@@ -153,24 +158,107 @@ function BattleFieldTiles({
         throw new Error("Container is not tall enough!");
     }
 
-    const leftPadding = freeWidth / (tiles.length + 1); // + 1 so we have an equal margin to the right of the last tile
-    const topPadding = freeHeight / 2;
-    console.log("left padding", leftPadding);
+    const tileHorizontalGap = freeWidth / (tiles.length + 1); // + 1 so we have an equal margin to the right of the last tile
+    const tileVerticalGap = freeHeight / 2;
 
+    function getBoundingRectForTile(tileIndex) {
+        /* Display index is based on which tiles are displayed, while tile index is the absolute index
+         if the fighters have moved to the right a few times, we might display tile indexes 4-9 like this:
+         4 5 6 7 8 9
+
+         But we need to do our calculations relative to what's visible, so tile index 4 here is tile display index 0
+       */
+        const tileDisplayIndex = tiles.findIndex(t => t.index === tileIndex);
+        if(tileDisplayIndex === -1) {
+            throw new Error(`Invalid tile! ${tileIndex}`);
+        }
+
+        const cumulativeLeftPadding = tileHorizontalGap * (tileDisplayIndex + 1);
+        const cumulativeTileWidth = tileSize * tileDisplayIndex;
+
+        return {
+            width: tileSize,
+            height: tileSize,
+            top: tileVerticalGap,
+            left: cumulativeTileWidth + cumulativeLeftPadding,
+        };
+    }
+
+    const fighterIdsByTile = {};
+    Object.keys(fighterLocations).forEach(fighterId => {
+        const tileIndex = fighterLocations[fighterId];
+
+        if(fighterIdsByTile[tileIndex] == null) {
+            fighterIdsByTile[tileIndex] = [];
+        }
+
+        fighterIdsByTile[tileIndex].push(fighterId);
+    });
+
+    type Position = {| +top: number, +left: number |};
+    function distributeFightersOnTile(fightersOnTile: $ReadOnlyArray<FighterType>): { [key: string]: Position} {
+        const allyFighters = fightersOnTile.filter(fighter => fighter.isAlly);
+        const enemyFighters = fightersOnTile.filter(fighter => !fighter.isAlly);
+
+        const fighterLocationsOnTile = {};
+
+        const spacingWhenTwoFighters = (tileSize - (fighterDisplaySize * 2)) / 3;
+        const spacingWhenOneFighter = (tileSize - fighterDisplaySize) / 2;
+
+        // Two teams, spread horizontally
+        let allyLeftPosition, enemyLeftPosition;
+        if(allyFighters.length > 0 && enemyFighters.length > 0) {
+            allyLeftPosition = spacingWhenTwoFighters;
+            enemyLeftPosition = spacingWhenTwoFighters + fighterDisplaySize + spacingWhenTwoFighters;
+        }
+        // One team, center horizontally
+        else {
+            allyLeftPosition = spacingWhenOneFighter;
+            enemyLeftPosition = spacingWhenOneFighter;
+        }
+
+        if(allyFighters.length === 2) {
+            fighterLocationsOnTile[allyFighters[0].id] = {
+                top: spacingWhenTwoFighters,
+                left: allyLeftPosition
+            }
+            fighterLocationsOnTile[allyFighters[1].id] = {
+                top: spacingWhenTwoFighters + fighterDisplaySize + spacingWhenTwoFighters,
+                left: allyLeftPosition
+            }
+        }
+        else if(allyFighters.length === 1) {
+            fighterLocationsOnTile[allyFighters[0].id] = {
+                top: spacingWhenOneFighter,
+                left: allyLeftPosition
+            }
+        }
+
+        if(enemyFighters.length === 2) {
+            fighterLocationsOnTile[enemyFighters[0].id] = {
+                top: spacingWhenTwoFighters,
+                left: enemyLeftPosition
+            }
+            fighterLocationsOnTile[enemyFighters[1].id] = {
+                top: spacingWhenTwoFighters + fighterDisplaySize + spacingWhenTwoFighters,
+                left: enemyLeftPosition
+            }
+        }
+        else if(enemyFighters.length === 1) {
+            fighterLocationsOnTile[enemyFighters[0].id] = {
+                top: spacingWhenOneFighter,
+                left: enemyLeftPosition
+            }
+        }
+
+        return fighterLocationsOnTile;
+    }
 
     return <React.Fragment>
-        {tiles.map((tile, i) => {
-            const cumulativeLeftPadding = leftPadding * (i + 1);
-            const cumulativeTileWidth = tileSize * i;
+        {tiles.map((tile) => {
+            const tileBoundingRect = getBoundingRectForTile(tile.index);
 
-            const tileStyles = {
-                width: tileSize,
-                height: tileSize,
-                top: topPadding,
-                left: cumulativeTileWidth + cumulativeLeftPadding,
-            };
-
-            return <div className="tileContainer" style={tileStyles} key={tile.index}>
+            return <div className="tileContainer" style={tileBoundingRect} key={tile.index}>
                 <BattleFieldTile
                     index={tile.index}
                     fighters={fightersForIds(tile.fighterIds)}
@@ -183,12 +271,44 @@ function BattleFieldTiles({
                 />
             </div>;
         })}
+        {Object.keys(fighterIdsByTile).map((tileIndexStr) => {
+            const tileIndex = parseInt(tileIndexStr);
+
+            const fighters = fightersForIds(fighterIdsByTile[tileIndex]);
+            if(fighters.length > 4) {
+                throw new Error(`Too many fighters on tile ${tileIndex}!`);
+            }
+
+            const tileBoundingRect = getBoundingRectForTile(tileIndex);
+
+            const fighterPositionsOnTile = distributeFightersOnTile(fighters);
+
+            return fighters.map(fighter => (
+                <div
+                    key={fighter.id}
+                    className={`tileFighter ${fighter.isAlly ? 'ally' : 'enemy'}`}
+                    style={{
+                        top: fighterPositionsOnTile[fighter.id].top + tileBoundingRect.top,
+                        left: fighterPositionsOnTile[fighter.id].left + tileBoundingRect.left,
+                        width: fighterDisplaySize,
+                        height: fighterDisplaySize,
+                    }}
+                >
+                    <FighterAvatar
+                        displaySize={fighterDisplaySize}
+                        fighterName={fighter.name}
+                        avatarLink={fighter.avatarLink}
+                        maxAvatarSize={20}
+                        includeContainer={false}
+                    />
+                </div>
+            ));
+        })}
     </React.Fragment>;
 }
 
 function BattleFieldTile({
     index,
-    fighters,
     canMoveTo,
     showAttackTarget,
     showAttackPreview,
@@ -215,16 +335,6 @@ function BattleFieldTile({
             onMouseLeave={onMouseLeave}
         >
             <span className='tileIndex'>{index}</span>
-            {fighters.map((fighter, i) => (
-                <div key={i} className={`tileFighter ${fighter.isAlly ? 'ally' : 'enemy'}`}>
-                    <FighterAvatar
-                        fighterName={fighter.name}
-                        avatarLink={fighter.avatarLink}
-                        maxAvatarSize={20}
-                        includeContainer={false}
-                    />
-                </div>
-            ))}
         </div>
     );
 }
