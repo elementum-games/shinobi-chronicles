66bcfb68891e6d09897c5d336743c9fab22b2e84
Tomasz Bawor <bawortomasz@gmail.com>
2022-12-11 12:51:50 +0100

    Add docker image for PHP

---
+++ secure/vars.docker.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Make a copy of this as vars.php if you are using docker
+ */
+
+$ENVIRONMENT = 'dev';
+$web_url = 'http://localhost:8000/';
+
+$SC_OPEN = true;
+$register_open = true;
+
+$host = 'shinobi-chronicles-db:3306';
+$username = 'root';
+$password = 'P@ssw0rd';
+$database = 'test';
