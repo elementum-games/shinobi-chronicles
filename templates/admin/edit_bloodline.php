409a46bce06c62e7822193a18f6fc19f2d1546e8
Levi Meahan <lsmjudoka@gmail.com>
2023-01-07 23:54:51 -0800

    Fix bloodline edit form

--- templates/admin/edit_bloodline.php
+++ templates/admin/edit_bloodline.php
@@ -3,15 +3,15 @@
  * @var string $admin_panel_url
  * @var array $bloodline_data
  * @var array $variables the allowed constraints for bloodline fields
+ * @var int $editing_bloodline_id
  */
 ?>
 <table class='table'>
     <tr><th>Edit Bloodline (<?= stripslashes($bloodline_data['name']) ?>)</th></tr>
     <tr><td>
-        <form action='<?= $admin_panel_url ?>&page=edit_bloodline' method='post'>
+        <form action='<?= $admin_panel_url ?>&page=edit_bloodline&bloodline_id=<?= $editing_bloodline_id ?>' method='post'>
             <?php displayFormFields($variables, $bloodline_data) ?>
             <br />
-            <input type='hidden' name='bloodline_id' value='{$editing_bloodline_id}' />
             <input type='submit' name='bloodline_data' value='Edit' />
         </form>
     </td></tr>
