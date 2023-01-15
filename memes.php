446668b0831e5c07e2e70552441c4c10eb95dfd1
AlexKJones <akjones1992@Yahoo.com>
2023-01-14 19:44:14 -0600

    Uraraka in game: Added a few custom made memes and also added a layout titled rainbow_road using a pastel theme. Hope you like it!

--- memes.php
+++ memes.php
@@ -450,4 +450,58 @@ return [
         'text' => ':feelschatman:',
         'public' => true,
     ],
+    [
+            'code' => ':shukaku_kun:',
+            'image' => "<img src='./images/memes/shukaku_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':shukaku_kun:',
+            'public' => true,
+    ],
+    [
+            'code' => ':matatabi_kun:',
+            'image' => "<img src='./images/memes/matatabi_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':matatabi_kun:',
+            'public' => true,
+    ],
+    [
+            'code' => ':isobu_kun:',
+            'image' => "<img src='./images/memes/isobu_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':isobu_kun:',
+            'public' => true,
+    ],
+    [
+            'code' => ':goku_kun:',
+            'image' => "<img src='./images/memes/goku_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':goku_kun:',
+            'public' => true,
+    ],
+    [
+            'code' => ':kokuo_kun:',
+            'image' => "<img src='./images/memes/kokuo_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':kokuo_kun:',
+            'public' => true,
+    ],
+    [
+            'code' => ':saiken_kun:',
+            'image' => "<img src='./images/memes/saiken_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':saiken_kun:',
+            'public' => true,
+    ],
+    [
+            'code' => ':chomei_kun:',
+            'image' => "<img src='./images/memes/chomei_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':chomei_kun:',
+            'public' => true,
+    ],
+    [
+            'code' => ':gyuki_kun:',
+            'image' => "<img src='./images/memes/gyuki_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':gyuki_kun:',
+            'public' => true,
+    ],
+    [
+            'code' => ':kurama_kun:',
+            'image' => "<img src='./images/memes/kurama_kun.png' style='width:64px;height:64px;' />",
+            'text' => ':kurama_kun:',
+            'public' => true,
+    ],
 ];
