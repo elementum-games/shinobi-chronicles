804984abac158e8cf5a639708425e474d37e1e97
Alvin V <alvin.vlnz14@gmail.com>
2023-01-15 14:52:11 -0800

    Update ajax_notifications.php

--- ajax_notifications.php
+++ ajax_notifications.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION['user_id'])) {
 require_once("classes.php");
 $system = new System();
 $player = new User($_SESSION['user_id']);
-$player->loadData(User::UPDATE_NOTHING
+$player->loadData(User::UPDATE_NOTHING);
 $playerInbox = new InboxManager($system, $player);
 $new_inbox_message = $playerInbox->checkIfUnreadMessages();
 $new_inbox_alerts = $playerInbox->checkIfUnreadAlerts();
