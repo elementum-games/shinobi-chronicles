59602a2a8ab61fc23b0990e5a6b27aa372fc91e4
Levi Meahan <lsmjudoka@gmail.com>
2023-03-18 22:07:29 -0700

    Nerf font regen to 100%

--- db/migrations/20230319045539_increase_map_size.php
+++ db/migrations/20230319045539_increase_map_size.php
@@ -7,7 +7,7 @@ final class IncreaseMapSize extends AbstractMigration {
     public function up(): void {
         $this->execute("UPDATE `maps_locations` SET `x` = 4, `y` = 12 WHERE `name` = 'Sand'");
         $this->execute("UPDATE `maps_locations` SET `x` = 7, `y` = 5 WHERE `name` = 'Stone'");
-        $this->execute("UPDATE `maps_locations` SET `x` = 10, `y` = 1 WHERE `name` = 'Font of Vitality'");
+        $this->execute("UPDATE `maps_locations` SET `x` = 10, `y` = 1, `regen`=100 WHERE `name` = 'Font of Vitality'");
         $this->execute("UPDATE `maps_locations` SET `x` = 15, `y` = 9 WHERE `name` = 'Leaf'");
         $this->execute("UPDATE `maps_locations` SET `x` = 16, `y` = 15 WHERE `name` = 'Underground Colosseum'");
         $this->execute("UPDATE `maps_locations` SET `x` = 24, `y` = 15 WHERE `name` = 'Mist'");
@@ -17,7 +17,7 @@ final class IncreaseMapSize extends AbstractMigration {
     public function down(): void {
         $this->execute("UPDATE `maps_locations` SET `x` = 3, `y` = 8 WHERE `name` = 'Sand'");
         $this->execute("UPDATE `maps_locations` SET `x` = 5, `y` = 3 WHERE `name` = 'Stone'");
-        $this->execute("UPDATE `maps_locations` SET `x` = 10, `y` = 1 WHERE `name` = 'Font of Vitality'");
+        $this->execute("UPDATE `maps_locations` SET `x` = 10, `y` = 1, `regen`=200 WHERE `name` = 'Font of Vitality'");
         $this->execute("UPDATE `maps_locations` SET `x` = 9, `y` = 6 WHERE `name` = 'Leaf'");
         $this->execute("UPDATE `maps_locations` SET `x` = 11, `y` = 9 WHERE `name` = 'Underground Colosseum'");
         $this->execute("UPDATE `maps_locations` SET `x` = 16, `y` = 10 WHERE `name` = 'Mist'");
