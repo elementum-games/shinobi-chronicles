10e3efc4c5da917622fa5d3954f2e9935f99a06d
Alberto Aguilera <drimer.aaj@gmail.com>
2023-03-22 23:02:25 +0000

    Make server container reach the DB

--- secure/vars.sample.php
+++ secure/vars.sample.php
@@ -11,7 +11,8 @@ $web_url = 'http://localhost/';
 $SC_OPEN = true;
 $register_open = true;
 
-$host = 'localhost';
+//Host needs to match the DB's host. For docker setup, rename it to "shinobi-chronicles-db".
+$host = 'locahost';
 $username = 'database_username';
 $password = 'database_password';
 $database = 'database_name';
