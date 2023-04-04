7b0246bc8f9db3e2f5d9ae449d43c94513d7a3c6
Alberto Aguilera <drimer.aaj@gmail.com>
2023-04-04 20:22:50 +0100

    Fix hardcoced react dependencies from unpkg.com

--- templates/inbox.php
+++ templates/inbox.php
@@ -24,8 +24,8 @@ if($player->checkBan(StaffManager::BAN_TYPE_PM)) {
 <div id="inboxReactContainer"></div>
 
 
-<script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
-<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+<script src="../node_modules/react/cjs/react.development.js" crossorigin></script>
+<script src="../node_modules/react-dom/cjs/react-dom.development.js" crossorigin></script>
 <script type="module" src="<?= $system->link?><?= $system->getReactFile("inbox/inbox") ?>"></script>
 <script>
     const inboxContainer = document.querySelector("#inboxReactContainer");
