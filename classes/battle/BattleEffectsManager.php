1a8c7f434ece1fec1501c13da101c423249dcaf6
aumu1031 <129538454+aumu1031@users.noreply.github.com>
2023-04-28 01:04:53 -0400

    Consolidate speed/cspeed nerf initial change (#292)
    Co-authored-by: aumu1031 <Audrey>

--- classes/battle/BattleEffectsManager.php
+++ classes/battle/BattleEffectsManager.php
@@ -82,7 +82,6 @@ class BattleEffectsManager {
             case 'speed_boost':
             case 'cast_speed_boost':
             case 'speed_nerf':
-            case 'cast_speed_nerf':
             case 'cripple':
                 // No changes needed to base number, calculated in applyPassiveEffects
                 break;
@@ -249,11 +248,9 @@ class BattleEffectsManager {
         else if($effect->effect == 'genjutsu_nerf') {
             $target->genjutsu_nerf += $effect_amount;
         }
-        else if($effect->effect == 'cast_speed_nerf') {
-            $target->cast_speed_nerf += $target->cast_speed * ($effect->effect_amount / 100);
-        }
         else if($effect->effect == 'speed_nerf' or $effect->effect == 'cripple') {
             $target->speed_nerf += $target->speed * ($effect->effect_amount / 100);
+            $target->cast_speed_nerf += $target->cast_speed * ($effect->effect_amount / 100);
         }
         else if($effect->effect == 'intelligence_nerf' or $effect->effect == 'daze') {
             $target->intelligence_nerf += $effect_amount;
