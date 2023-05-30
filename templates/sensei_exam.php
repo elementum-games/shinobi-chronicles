914b2f9cb7cbb430b6c0dd21f71d639e4fcd66f7
aumu1031 <Audrey>
2023-05-30 00:29:36 -0400

    Update sensei exam question: speed

--- templates/sensei_exam.php
+++ templates/sensei_exam.php
@@ -53,12 +53,11 @@
                 <div><input type="radio" name="question3" value="3b" /> <label>Equal Offense/Speed Stats</label></div>
                 <div><input type="radio" name="question3" value="3c" /> <label>Equal Bloodline/Offense/Speed Stats</label></div>
                 <div><input type="radio" name="question3" value="3d" /> <label>None of the above</label></div>
-                <p class="question_label">How much speed is required to reach maximum evasion (35%) against 80,000 speed?</p>
-                <div><input type="radio" name="question4" value="4a" checked/> <label>100,000</label></div>
-                <div><input type="radio" name="question4" value="4b" /> <label>125,000</label></div>
-                <div><input type="radio" name="question4" value="4c" /> <label>150,000</label></div>
-                <div><input type="radio" name="question4" value="4d" /> <label>175,000</label></div>
-                <div><input type="radio" name="question4" value="4e" /> <label>200,000</label></div>
+                <p class="question_label">How much speed is required to reach maximum evasion (35%)?</p>
+                <div><input type="radio" name="question4" value="4a" checked/> <label>+50%</label></div>
+                <div><input type="radio" name="question4" value="4b" /> <label>+66.6%</label></div>
+                <div><input type="radio" name="question4" value="4c" /> <label>+87.5%</label></div>
+                <div><input type="radio" name="question4" value="4d" /> <label>+100%</label></div>
                 <p class="question_label">Which of the following jutsu has the greatest total power?</p>
                 <div><input type="radio" name="question5" value="5a" checked/> <label>3.5 Base Power, lv100 - Shop Jutsu</label></div>
                 <div><input type="radio" name="question5" value="5b" /> <label>3.5 Base Power, lv100 - Bloodline Jutsu</label></div>
