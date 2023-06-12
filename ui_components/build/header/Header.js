4db048711443e56de8afeb33277f2438e11baf13
Levi Meahan <lsmjudoka@gmail.com>
2023-06-11 19:51:15 -0700

    Header build

--- ui_components/build/header/Header.js
+++ ui_components/build/header/Header.js
@@ -1,11 +1,11 @@
-import { apiFetch } from "../utils/network.js"; // Initialize
+import { apiFetch } from "../utils/network.js";
 
 function Header({
   links,
   navigationAPIData
 }) {
   // Hooks
-  const [headerMenu, setHeaderMenu] = React.useState(navigationAPIData.headerMenu);
+  const [headerMenuLinks, setHeaderMenuLinks] = React.useState(navigationAPIData.headerMenu);
   const [serverTime, setServerTime] = React.useState(null); // API
 
   function getHeaderMenu() {
@@ -15,47 +15,26 @@ function Header({
       if (response.errors.length) {
         handleErrors(response.errors);
         return;
-      } else {
-        setHeaderMenu(response.data.headerMenu);
       }
+
+      setHeaderMenuLinks(response.data.headerMenu);
     });
   } // Utility
 
 
   function getCurrentTime() {
-    var currentDate = new Date();
-    var options = {
+    const currentDate = new Date();
+    const options = {
       weekday: 'long',
       year: 'numeric',
       month: 'short',
       day: 'numeric'
     };
-    var formattedDate = currentDate.toLocaleDateString('en-US', options);
-    var formattedTime = currentDate.toLocaleTimeString('en-US', {
+    const formattedDate = currentDate.toLocaleDateString('en-US', options);
+    const formattedTime = currentDate.toLocaleTimeString('en-US', {
       hour12: true
     });
     setServerTime(formattedDate + ' - ' + formattedTime);
-  } // Content
-
-
-  function displayHeader(headerData, serverTime) {
-    return /*#__PURE__*/React.createElement("div", {
-      className: "header_bar"
-    }, /*#__PURE__*/React.createElement("div", {
-      className: "header_bar_inner"
-    }, /*#__PURE__*/React.createElement("div", {
-      className: "header_link_container d-flex"
-    }, headerData && headerData.map(function (link, i) {
-      return /*#__PURE__*/React.createElement("div", {
-        key: i,
-        className: "header_link_wrapper t-center"
-      }, /*#__PURE__*/React.createElement("a", {
-        href: link.url,
-        className: "header_label ft-default ft-s ft-c5"
-      }, link.title));
-    }), /*#__PURE__*/React.createElement("div", {
-      className: "header_time_label ft-default ft-s ft-c5"
-    }, serverTime))));
   } // Misc
 
 
@@ -74,7 +53,21 @@ function Header({
 
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
     className: "header_bar_left"
-  }), headerMenu && displayHeader(headerMenu, serverTime));
+  }), headerMenuLinks && /*#__PURE__*/React.createElement("div", {
+    className: "header_bar"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "header_link_container d-flex"
+  }, headerMenuLinks && headerMenuLinks.map(function (link, i) {
+    return /*#__PURE__*/React.createElement("div", {
+      key: i,
+      className: "header_link_wrapper t-center"
+    }, /*#__PURE__*/React.createElement("a", {
+      href: link.url,
+      className: "header_label ft-default ft-s ft-c5"
+    }, link.title));
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "header_time_label ft-default ft-s ft-c5"
+  }, serverTime))));
 }
 
 window.Header = Header;
