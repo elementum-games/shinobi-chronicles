586bc1c00bcac73733758237ddecb4e6682bda9d
aumu1031 <129538454+aumu1031@users.noreply.github.com>
2023-06-14 02:47:52 -0400

    Update support.php
    Co-authored-by: Levi Meahan <lsmjudoka@gmail.com>

--- support.php
+++ support.php
@@ -197,6 +197,7 @@ if($player != null) {
                     }
                 }catch (Exception $e) {
                     $system->rollbackTransaction();
+                    error_log($e->getMessage());
                     $system->message($e->getMessage());
                 }
             }
