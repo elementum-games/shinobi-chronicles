6f2e763a7e7733205574bec1b3f97bd281aa85dd
aumu1031 <Audrey>
2023-06-20 06:23:52 -0400

    Fix migration

--- db/migrations/20230620062917_sensei_lesson_migration.php
+++ db/migrations/20230620062917_sensei_lesson_migration.php
@@ -13,11 +13,28 @@ final class SenseiLessonMigration extends AbstractMigration
         // Modify sensei table
         $this->execute("
             ALTER TABLE `sensei`
-                ADD `temp_students` VARCHAR(50) NOT NULL DEFAULT '[]' ,
+                ADD `temp_students` VARCHAR(50) NOT NULL DEFAULT '[]',
                 ADD `time_trained` INT(13) NOT NULL DEFAULT '0',
-                ADD `yen_gained` INT(11) NOT NULL DEFAULT '0'`
-                ADD `is_active` TINYINT(1) NOT NULL DEFAULT '1'
+                ADD `yen_gained` INT(11) NOT NULL DEFAULT '0',
+                ADD `is_active` TINYINT(1) NOT NULL DEFAULT '1',
                 ADD `enable_lessons` TINYINT(1) NOT NULL DEFAULT '1';
         ");
     }
+
+    /**
+     * Migrate Down.
+     */
+    public function down(): void
+    {
+        // Modify sensei table
+        $this->execute("
+            ALTER TABLE `sensei`
+                DROP `temp_students`,
+                DROP `time_trained`,
+                DROP `yen_gained`,
+                DROP `is_active`,
+                DROP `enable_lessons`;
+        ");
+    }
+
 }
