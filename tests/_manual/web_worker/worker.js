daf6ebc6e6cd00e53c9df7b8bce4329922adcf00
Levi Meahan <lsmjudoka@gmail.com>
2023-07-06 18:05:58 -0700

    Web worker test

---
+++ tests/_manual/web_worker/worker.js
@@ -0,0 +1,15 @@
+onmessage = function(e) {
+    console.log('[worker] starting count', e);
+
+    let workerCount = 0;
+    const workerIntervalId = setInterval(incrementWorkerCount, 1000);
+    function incrementWorkerCount() {
+        if(workerCount >= 1000) {
+            clearInterval((workerIntervalId));
+            return;
+        }
+
+        workerCount++;
+        postMessage({ newCount: workerCount });
+    }
+}
