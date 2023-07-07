2834943ee020b1743da3002a9ffbf8030d048bcf
Levi Meahan <lsmjudoka@gmail.com>
2023-07-06 18:30:56 -0700

    Update worker example to more accurately reflect special missions

--- tests/_manual/web_worker/worker.js
+++ tests/_manual/web_worker/worker.js
@@ -2,7 +2,7 @@ onmessage = function(e) {
     console.log('[worker] starting count', e);
 
     let workerCount = 0;
-    const workerIntervalId = setInterval(incrementWorkerCount, 1000);
+    const workerIntervalId = setInterval(incrementWorkerCount, 800);
     function incrementWorkerCount() {
         if(workerCount >= 1000) {
             clearInterval((workerIntervalId));
