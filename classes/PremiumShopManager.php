9560f82c4376bc0e3f944b28a01b61c7dd25687e
Kelly Baillie <kbaillieworks@gmail.com>
2023-07-24 12:57:26 -0700

    Amend daily task reset

--- classes/PremiumShopManager.php
+++ classes/PremiumShopManager.php
@@ -155,9 +155,6 @@ class PremiumShopManager {
         $this->player->pvp_rep = 0;
         $this->player->mission_rep_cd = 0;
 
-        //Force roll new daily task
-        $this->player->daily_tasks_reset = 0;
-
         //Bug fix: Elements previously was not cleared. -- Shadekun
         $this->player->elements = array();
         $this->player->missions_completed = array(); //Reset missions complete -- Hitori
@@ -167,6 +164,7 @@ class PremiumShopManager {
         $this->player->updateData();
 
         $this->system->db->query("DELETE FROM `user_bloodlines` WHERE `user_id`='{$this->player->user_id}'");
+        $this->system->db->query("UPDATE `daily_tasks` SET `last_reset`='0' WHERE `user_id`='{$this->player->user_id}'");
         $this->system->db->query(
             "UPDATE `user_inventory` SET
                     `jutsu` = '',
