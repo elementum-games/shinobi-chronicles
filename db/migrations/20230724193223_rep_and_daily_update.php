744e0902ecd598bcc4eb5dd9afc6b4fb5437321b
Kelly Baillie <kbaillieworks@gmail.com>
2023-07-24 12:33:45 -0700

    Convert manually crated migration to generated version

--- db/migrations/20230724193223_rep_and_daily_update.php
+++ db/migrations/20230724193223_rep_and_daily_update.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class PvpRepUpdate extends AbstractMigration
+final class RepAndDailyUpdate extends AbstractMigration
 {
     /**
      * Change Method.
