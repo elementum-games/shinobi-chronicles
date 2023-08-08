d14d9441fc46c5621797465a43da05d33723338c
Kelly Baillie <anubiswork@gmail.com>
2023-08-08 01:09:20 -0700

    Update TrainingManager.php
    Skill/Attr clan boost fix

--- classes/training/TrainingManager.php
+++ classes/training/TrainingManager.php
@@ -228,10 +228,8 @@ class TrainingManager {
             // Clan boost info
             $clan_boost = false;
             if (isset($this->clan)) {
-                if ($this->clan->boost_type == 'training') {
-                    if ($this->train_type == $this->clan->boost_effect) {
-                        $clan_boost = $this->clan->boost_amount;
-                    }
+                if (str_contains($this->clan->boost_type, 'training:') && $this->train_type == explode(":", $this->clan->boost_effect)[1]) {
+                	$clan_boost = $this->clan->boost_amount;
                 }
             }
             // Length logic
