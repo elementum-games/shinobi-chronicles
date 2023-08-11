62b7a852c80313b34e58da3a7601e132bd9b9868
Kelly Baillie <kbaillieworks@gmail.com>
2023-08-11 01:04:05 -0700

    Fix broken home page for news created in news.php

--- classes/news/NewsAPIPresenter.php
+++ classes/news/NewsAPIPresenter.php
@@ -10,7 +10,12 @@ class NewsApiPresenter {
     public static function newsPostResponse(NewsManager $newsManager, System $system, int $num_posts = 8): array {
         return array_map(
             function (NewsPostDto $post) use ($system) {
-                $message = $system->parseMarkdown(stripslashes($post->message), true);
+                if(!System::LOCAL_HOST) {
+                    $message = $system->parseMarkdown(stripslashes($post->message), true);
+                }
+                else {
+                    $message = nl2br(stripslashes($post->message));
+                }
 
                 return [
                     'post_id' => $post->post_id,
