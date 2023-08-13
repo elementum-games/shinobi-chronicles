980a87f0cd09c1c58ef86723c9d83c929f329100
Kelly Baillie <anubiswork@gmail.com>
2023-08-13 02:39:07 -0700

    Update Currency.php
    Currency improvements

--- classes/Currency.php
+++ classes/Currency.php
@@ -12,23 +12,39 @@ class Currency {
     const PREMIUM_SYMBOL = "AK";
     const TOKEN_SYMBOL = "AS";
 
+    public array $valid_currency_types;
+    public string $name;
+    public string $symbol;
+
     public function __construct(
+        // Defined members
         public System $system,
         public string $type,
-        private ?int $user_id,
+        private int $user_id,
         private int $amount,
-
         public ?UserDailyTasks &$userDailyTasks = null,
-        public ?int $max_amount = null,
-        public array $valid_types = [
-            self::TYPE_MONEY, self::TYPE_PREMIUM_CREDITS, self::TYPE_TOKEN
-        ]
+        public ?int $max_amount = null
     ) {
-        if(!in_array($this->type, $this->valid_types)) {
-            throw new RuntimeException("Invalid currency type!");
+        $this->valid_currency_types = self::getValidCurrencies();
+        if(!in_array($this->type, $this->valid_currency_types)) {
+            throw new RunetimeException("Invalid currency type {$this->type}!");
         }
+            
+        $this->name = self::getCurrencyName($this->type);
+        $this->symbol = self::getCurrencySymbol($this->type);
     }
 
+    // Validate currency type and set default members
+    public function setDefaults() {
+        $this->valid_currency_types = self::getCurrencyTypes();
+        if(!in_array($this->type, $this->valid_currency_types)) {
+            throw new RunetimeException("Invalid currency type {$this->type}!");
+        }
+        $this->name = self::getCurrencyName($this->type);
+        $this->symbol = self::getCurrencySymbol($this->type);
+    }
+
+    // Process currency increase, set amount and log currency
     public function add(int $amount, string $description, bool $increment_daily_task = true): void {
         if($increment_daily_task && $this->type == self::TYPE_MONEY && $this->userDailyTasks instanceof UserDailyTasks) {
             $this->userDailyTasks->progressTask(DailyTask::ACTIVITY_EARN_MONEY, $amount);
@@ -39,6 +55,7 @@ class Currency {
         $this->set($this->amount + $amount, $description);
     }
 
+    // Process currency reduction, set amount and log transaction
     public function subtract(int $amount, string $description) {
         if($this->amount - $amount < 0) {
             throw new RuntimeException("Not enough " . System::unSlug($this->type) . "!");
@@ -46,6 +63,7 @@ class Currency {
         $this->set($this->amount - $amount, $description);
     }
 
+    // Set currency to new amount and log transaction
     public function set(int $new_amount, string $description): void {
         $this->system->currencyLog(
             character_id: $this->user_id,
@@ -57,16 +75,26 @@ class Currency {
         );
         $this->amount = $new_amount;
     }
+
+    // Return currnt amount of currency
     public function getAmount(): int {
         return $this->amount;
     }
+
+    /** Currency Conventions **/
+    public static function getValidCurrencies(): array {
+        return [self::TYPE_MONEY, self::TYPE_PREMIUM_CREDITS, /*self::TYPE_TOKEN*/];
+    }
+    
     public function getName(): string {
         return self::getCurrencyName($this->type);
     }
+    
     public function getSymbol(): string {
         return self::getCurrencySymbol($this->type);
     }
-    public static function getCurrencyName($type): string {
+    
+    public static function getCurrencyName(string $type): string {
         return match ($type) {
             self::TYPE_MONEY => self::MONEY_NAME,
             self::TYPE_PREMIUM_CREDITS => self::PREMIUM_NAME,
@@ -74,7 +102,8 @@ class Currency {
             default => '???',
         };
     }
-    public static function getCurrencySymbol($type): string {
+    
+    public static function getCurrencySymbol(string $type): string {
         return match ($type) {
             self::TYPE_MONEY => self::MONEY_SYMBOL,
             self::TYPE_PREMIUM_CREDITS => self::PREMIUM_SYMBOL,
@@ -83,7 +112,24 @@ class Currency {
         };
     }
 
-    public static function getValidCurrencies(): array {
-        return [self::TYPE_MONEY, self::TYPE_PREMIUM_CREDITS, self::TYPE_TOKEN];
+    /** Calculate Yen Gains **/
+    public static function calcRawYenGain(int $rank_num, int $multiplier): int {
+        $gain = ceil(((30 * $rank_num) + pow($rank_num+1, 2)) * $multiplier);
+        return $gain;
+    }
+
+    public static function roundYen(int $num, int $multiple_of) {
+        $remainder = $num % $multiple_of;
+        if($num / $multiple_of >= 0.5) {
+            return $num + ($multiple_of - $remainder);
+        }
+        return $num - $remainder;
+    }
+
+    public static funcion getRoundedYen(int $rank_num, int $multiplier, int $multiple_of) {
+        return self::roundYen(
+            num: self::calcRawYenGain($rank_num, $multiplier),
+            multiple_of: $multiple_of
+        );
     }
 }
