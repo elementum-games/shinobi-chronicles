5c0896fa59b48ee2af5e1676be1684a6d882a0ea
Kelly Baillie <anubiswork@gmail.com>
2023-08-22 06:00:06 +0000

    Yen helper

---
+++ admin/money_helper.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @var System $system
+ * @var User $user
+ */
+require_once __DIR__ . "/_authenticate_admin.php";
+$self_link = $system->router->base_url . 'admin/money_helper.php';
+$DISPLAY_DATA = [];
+?>
+
+<?php if(!empty($DISPLAY_DATA)): ?>
+    Rank: <?=$DISPLAY_DATA['rank']?><br />
+    Multiplier: <?=$DISPLAY_DATA['multiplier']?><br />
+    Multiple: <?=$DISPLAY_DATA['multiple_of']?><br />
+    Raw: <?=Currency::calcRawYenGain(rank_num: $DISPLAY_DATA['rank_num'], multiplier: $DISPLAY_DATA['multiplier'])?><br />
+    Final Result: <?=Currency::getRoundedYen(rank_num: $DISPLAY_DATA['rank_num'], multiplier: $DISPLAY_DATA['multiplier'], multiple_of: $DISPLAY_DATA['multiple_of'])?>
+<?php endif ?>
+<form action="<?=$self_link?>" method="post">
+    Rank: <select name="rank">
+        <option value='1'>Akademi-sai</option>
+        <option value='2'>Genin</option>
+        <option value='3'>Chuunin</option>
+        <option value='4'>Jonin</option>
+        <option value='5'>Sennin</option>
+    </select><br />
+    Multiplier: <input type='text' name='multiplier' /><br />
+    Multiple Of: <input type='text' name='multiple_of' /><br />
+    <input type='submit' name='calc_gain' value='Run' />
+</form>
