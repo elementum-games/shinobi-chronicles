78823d30cf2d329f62664982958c263f55016eca
Arthesia <129538454+aumu1031@users.noreply.github.com>
2023-08-26 23:14:12 -0400

    Map Update fixes (#410)
    * custom border image
    
    * Use map_location for village location
    
    * fix
    
    * Region colors
    
    * capitalization
    
    ---------
    
    Co-authored-by: aumu1031 <Audrey>

--- classes/travel/TravelManager.php
+++ classes/travel/TravelManager.php
@@ -36,7 +36,9 @@ class TravelManager {
 
     public static function locationIsInVillage(System $system, TravelCoords $location): bool {
         $result = $system->db->query(
-            "SELECT COUNT(*) as `count` FROM `villages` WHERE `location`='{$location->fetchString()}' LIMIT 1"
+            "SELECT COUNT(*) as `count` FROM `villages`
+            INNER JOIN `maps_locations` ON `maps_locations`.`location_id` = `villages`.`map_location_id`
+            WHERE `x`='{$location->x}' AND `y`='{$location->y}' AND `map_id`='{$location->map_id}' LIMIT 1"
         );
         $count = (int)$system->db->fetch($result)['count'];
 
