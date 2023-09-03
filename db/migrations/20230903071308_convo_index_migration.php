6a632bb7aab3dd715d0ef3f086f2668a058bbaaa
aumu1031 <Audrey>
2023-09-03 03:37:28 -0400

    Fix Migration Inbox

--- db/migrations/20230903071308_convo_index_migration.php
+++ db/migrations/20230903071308_convo_index_migration.php
@@ -17,9 +17,11 @@ final class ConvoIndexMigration extends AbstractMigration
         $this->execute("ALTER TABLE `convos_users` ADD INDEX(`user_id`)");
         $this->execute("ALTER TABLE `convos_users` ADD INDEX(`last_read`)");
         $this->execute("ALTER TABLE `convos_users` ADD INDEX(`muted`)");
+        $this->execute("ALTER TABLE `convos_users` ADD INDEX(`convo_id`)");
 
         // Modify Convo_Messages table
         $this->execute("ALTER TABLE `convos_messages` ADD INDEX(`time`)");
+        $this->execute("ALTER TABLE `convos_messages` ADD INDEX(`convo_id`)");
 
         // Commit Transaction
         $this->execute("COMMIT");
@@ -37,9 +39,11 @@ final class ConvoIndexMigration extends AbstractMigration
         $this->execute("DROP INDEX `user_id` ON `convos_users`");
         $this->execute("DROP INDEX `last_read` ON `convos_users`");
         $this->execute("DROP INDEX `muted` ON `convos_users`");
+        $this->execute("DROP INDEX `convo_id` ON `convos_users`");
 
         // Modify Convo_Messages table
         $this->execute("DROP INDEX `time` ON `convos_messages`");
+        $this->execute("DROP INDEX `convo_id` ON `convos_messages`");
 
         // Commit Transaction
         $this->execute("COMMIT");
