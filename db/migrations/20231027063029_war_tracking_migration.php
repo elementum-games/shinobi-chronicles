56b6d3e01ff695956833223b119057242266e7d5
aumu1031 <Audrey>
2023-10-28 00:52:38 -0400

    Region names

--- db/migrations/20231027063029_war_tracking_migration.php
+++ db/migrations/20231027063029_war_tracking_migration.php
@@ -84,6 +84,27 @@ final class WarTrackingMigration extends AbstractMigration
             INSERT INTO `patrols` (`id`, `start_time`, `travel_time`, `travel_interval`, `region_id`, `village_id`, `name`, `ai_id`, `tier`) VALUES (NULL, '1698454683', NULL, '2000 ', '18', '5', 'Patrol', '8', '1');
             INSERT INTO `patrols` (`id`, `start_time`, `travel_time`, `travel_interval`, `region_id`, `village_id`, `name`, `ai_id`, `tier`) VALUES (NULL, '1698454683', NULL, '2000 ', '19', '5', 'Patrol', '8', '1');
             INSERT INTO `patrols` (`id`, `start_time`, `travel_time`, `travel_interval`, `region_id`, `village_id`, `name`, `ai_id`, `tier`) VALUES (NULL, '1698454683', NULL, '2000 ', '20', '5', 'Patrol', '8', '1');
+
+            UPDATE `regions` SET `name` = 'Verdant Plateau' WHERE `region_id` = 1;
+            UPDATE `regions` SET `name` = 'Stormveil Peaks' WHERE `region_id` = 2;
+            UPDATE `regions` SET `name` = 'Ashen Forest' WHERE `region_id` = 3;
+            UPDATE `regions` SET `name` = 'Windswept Barrens' WHERE `region_id` = 4;
+            UPDATE `regions` SET `name` = 'Shrouded Isles' WHERE `region_id` = 5;
+            UPDATE `regions` SET `name` = 'Stillwind Shore' WHERE `region_id` = 6;
+            UPDATE `regions` SET `name` = 'Gradient Plains' WHERE `region_id` = 7;
+            UPDATE `regions` SET `name` = 'Fortune''s Pass' WhERE `region_id` = 8;
+            UPDATE `regions` SET `name` = 'Cerulean Cliffs' WHERE `region_id` = 9;
+            UPDATE `regions` SET `name` = 'Royal Valley' WHERE `region_id` = 10;
+            UPDATE `regions` SET `name` = 'World''s End' WHERE `region_id` = 11;
+            UPDATE `regions` SET `name` = 'Whispering Woods' WHERE `region_id` = 12;
+            UPDATE `regions` SET `name` = 'Eternal Plains' WHERE `region_id` = 13;
+            UPDATE `regions` SET `name` = 'Ancient''s Woodland' WHERE `region_id` = 14;
+            UPDATE `regions` SET `name` = 'Shimmering Sands' WHERE `region_id` = 15;
+            UPDATE `regions` SET `name` = 'Hero''s Steppe' WHERE `region_id` = 16;
+            UPDATE `regions` SET `name` = 'Fighter''s Bay' WHERE `region_id` = 17;
+            UPDATE `regions` SET `name` = 'Silent Depths' WHERE `region_id` = 18;
+            UPDATE `regions` SET `name` = 'Crescent Bay' WHERE `region_id` = 19;
+            UPDATE `regions` SET `name` = 'Heaven''s Reach' WHERE `region_id` = 20;
         ");
     }
 
@@ -101,6 +122,28 @@ final class WarTrackingMigration extends AbstractMigration
 
             -- Drop table region_logs
             DROP TABLE `region_logs`;
+
+            -- Alter table regions
+            UPDATE `regions` SET `name` = 'Stone' WHERE `region_id` = 1;
+            UPDATE `regions` SET `name` = 'Cloud' WHERE `region_id` = 2;
+            UPDATE `regions` SET `name` = 'Leaf' WHERE `region_id` = 3;
+            UPDATE `regions` SET `name` = 'Sand' WHERE `region_id` = 4;
+            UPDATE `regions` SET `name` = 'Mist' WHERE `region_id` = 5;
+            UPDATE `regions` SET `name` = 'Stone East' WHERE `region_id` = 6;
+            UPDATE `regions` SET `name` = 'Stone Southeast' WHERE `region_id` = 7;
+            UPDATE `regions` SET `name` = 'Stone South' WHERE `region_id` = 8;
+            UPDATE `regions` SET `name` = 'Cloud South' WHERE `region_id` = 9;
+            UPDATE `regions` SET `name` = 'Could Southwest' WHERE `region_id` = 10;
+            UPDATE `regions` SET `name` = 'Cloud West' WHERE `region_id` = 11;
+            UPDATE `regions` SET `name` = 'Leaf North' WHERE `region_id` = 12;
+            UPDATE `regions` SET `name` = 'Leaf East' WHERE `region_id` = 13;
+            UPDATE `regions` SET `name` = 'Leaf South' WHERE `region_id` = 14;
+            UPDATE `regions` SET `name` = 'Sand North' WHERE `region_id` = 15;
+            UPDATE `regions` SET `name` = 'Sand East' WHERE `region_id` = 16;
+            UPDATE `regions` SET `name` = 'Sand Southeast' WHERE `region_id` = 17;
+            UPDATE `regions` SET `name` = 'Mist North' WHERE `region_id` = 18;
+            UPDATE `regions` SET `name` = 'Mist West' WHERE `region_id` = 19;
+            UPDATE `regions` SET `name` = 'Mist Northwest' WHERE `region_id` = 20;
         ");
     }
 }
