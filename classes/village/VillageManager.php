3bcf0d15e549845d841a39962eeba33f80615bec
aumu1031 <Audrey>
2023-10-29 12:48:03 -0400

    vote hours

--- classes/village/VillageManager.php
+++ classes/village/VillageManager.php
@@ -32,7 +32,7 @@ class VillageManager {
     const MIN_ELDER_CHALLENGE_TIER = 4;
 
     // Set these to correct values for release
-    const PROPOSAL_VOTE_HOURS = 72; // 72
+    const PROPOSAL_VOTE_HOURS = 24; // 24
     const PROPOSAL_ENACT_HOURS = 24; // 24
     const PROPOSAL_COOLDOWN_HOURS = 1; // 1
     const KAGE_PROVISIONAL_DAYS = 7; // 7
