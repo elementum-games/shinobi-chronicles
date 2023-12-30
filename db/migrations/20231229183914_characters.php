14bc4f0f56e2c05b9e2b3fdb77050d409b53bd46
Kelly Baillie <anubiswork@gmail.com>
2023-12-30 11:25:53 -0800

    Update 20231229183914_characters.php
    Added reversibility to reports & user records

--- db/migrations/20231229183914_characters.php
+++ db/migrations/20231229183914_characters.php
@@ -9,12 +9,30 @@ final class Characters extends AbstractMigration
         $this->execute("
             -- Change user table to characters
             RENAME TABLE `users` TO `characters`;
+            
             -- Add character data into table
             ALTER TABLE `characters` DROP PRIMARY KEY, CHANGE `user_id` `user_id` INT NOT NULL;
             ALTER TABLE `characters`
                 ADD `character_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT FIRST;
+                
             -- Add indexing to character_id
             ALTER TABLE `characters` ADD INDEX (`staff_level`);
+
+            -- Add character_id to user_records
+            ALTER TABLE `user_record`
+                ADD `character_id` INT NOT NULL AFTER `user_id`;
+
+            -- Add character id to existing record, this is needed to maintain reversibility
+            UPDATE `user_record`
+                SET `character_id` = (SELECT `character_id` FROM `characters` WHERE `characters`.`user_id` = `user_record`.`user_id`);
+
+            -- Add character_id to reports
+            ALTER TABLE `reports`
+                ADD `character_id` INT NOT NULL AFTER `user_id`;
+
+            -- Add character id to existing reports, this is needed to maintain reversibility
+            UPDATE `reports`
+                SET `character_id` = (SELECT `character_id` FROM `characters` WHERE `characters`.`user_id` = `reports`.`user_id`);
             
             -- Create new users table
             CREATE TABLE `users` (
@@ -23,9 +41,11 @@ final class Characters extends AbstractMigration
                 `password` VARCHAR(256) NOT NULL,
                 `staff_level` SMALLINT(6) NOT NULL DEFAULT 0
             );
+            
             -- Populate new user table
             INSERT INTO `users` (`user_id`, `user_name`, `password`, `staff_level`)
             SELECT `user_id`, `user_name`, `password`, `staff_level` FROM `characters`;
+            
             -- Add indexing to users
             ALTER TABLE `users` ADD INDEX (`user_name`);
             
@@ -50,12 +70,25 @@ final class Characters extends AbstractMigration
                 DROP PRIMARY KEY,
                 DROP `user_id`,
                 CHANGE `character_id` `user_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT;
+
+            -- Update user record and attach them to appropriate user
+            ALTER TABLE `user_record`
+                DROP `user_id`,
+                CHANGE `character_id` `user_id` INT NOT NULL;
+
+            -- Update reports and attach them to appropriate user
+            ALTER TABLE `reports`
+                DROP `user_id`,
+                CHANGE `character_id` `user_id` INT NOT NULL;
                 
             -- Remove new users table
             DROP TABLE `users`;
             
             -- Revert characters back to users
             RENAME TABLE `characters` TO `users`;
+
+            -- Remove staff_level indexing from users
+            DROP INDEX `staff_level` ON `users`;
         ");
     }
 }
