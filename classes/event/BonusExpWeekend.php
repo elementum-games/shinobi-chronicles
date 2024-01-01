67bc275082bb69d8c48444858aa444c3f443de4c
Kelly Baillie <anubiswork@gmail.com>
2023-12-31 21:32:07 -0800

    Update BonusExpWeekend.php

--- classes/event/BonusExpWeekend.php
+++ classes/event/BonusExpWeekend.php
@@ -4,7 +4,8 @@ require_once __DIR__ . '/Event.php';
 
 class BonusExpWeekend extends Event
 {
-    public function __construct(DateTimeImmutable $end_time) {
+    public function __construct(DateTimeImmutable $end_time)
+    {
         $this->exp_gain_multiplier = 1.5;
         $this->name = "Weekend Bonus EXP";
         $this->end_time = $end_time;
