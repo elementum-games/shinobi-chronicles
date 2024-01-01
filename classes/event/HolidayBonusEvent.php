8ea334a5a3c86d659f14dcb3ee1fbd87c4418e0e
Kelly Baillie <anubiswork@gmail.com>
2023-12-31 21:32:46 -0800

    Update HolidayBonusEvent.php

--- classes/event/HolidayBonusEvent.php
+++ classes/event/HolidayBonusEvent.php
@@ -4,10 +4,10 @@ require_once __DIR__ . '/Event.php';
 
 class HolidayBonusEvent extends Event
 {
-    public function __construct(DateTimeImmutable $end_time, int $boost_amount = 2, string $name = "Holiday Bonus EXP")
+    public function __construct(DateTimeImmutable $end_time, int string $name = "Holiday Bonus EXP")
     {
         $this->name = $name;
         $this->end_time = $end_time;
-        $this->exp_gain_multiplier = $boost_amount;
+        $this->exp_gain_multiplier = 2;
     }
 }
