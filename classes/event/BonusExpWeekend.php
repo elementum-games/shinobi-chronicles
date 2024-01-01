827ddc36f8277980d9fb2a6386e310607d698672
Kelly Baillie <anubiswork@gmail.com>
2023-12-31 21:22:36 -0800

    Update BonusExpWeekend.php
    Revert changes to weekend bonus

--- classes/event/BonusExpWeekend.php
+++ classes/event/BonusExpWeekend.php
@@ -4,10 +4,9 @@ require_once __DIR__ . '/Event.php';
 
 class BonusExpWeekend extends Event
 {
-    public function __construct(DateTimeImmutable $end_time, string $name = "Weekend Bonus EXP", int $exp_modifier = 1.5)
-    {
-        $this->exp_gain_multiplier = $exp_modifier;
-        $this->name = $name;
+    public function __construct(DateTimeImmutable $end_time) {
+        $this->exp_gain_multiplier = 1.5;
+        $this->name = "Weekend Bonus EXP";
         $this->end_time = $end_time;
     }
 }
