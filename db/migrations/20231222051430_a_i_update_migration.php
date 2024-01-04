cf6d10410c6193debb4904a5394947ca60f96c32
Audrey Mueller <audrey.mueller2@gmail.com>
2024-01-03 22:27:39 -0500

    Fix

--- db/migrations/20231222051430_a_i_update_migration.php
+++ db/migrations/20231222051430_a_i_update_migration.php
@@ -16,7 +16,7 @@ final class AIUpdateMigration extends AbstractMigration
             ALTER TABLE `ai_opponents` ADD `shop_jutsu_priority` VARCHAR(100) NOT NULL DEFAULT '0';
             ALTER TABLE `ai_opponents` ADD `battle_iq` INT(11) NOT NULL DEFAULT '0';
             ALTER TABLE `ai_opponents` ADD `scaling` TINYINT(1) NOT NULL DEFAULT '0';
-            ALTER TABLE `ai_opponents` ADD `difficulty_level` VARCHAR(20) NOT NULL DEFAULT 'none',
+            ALTER TABLE `ai_opponents` ADD `difficulty_level` VARCHAR(20) NOT NULL DEFAULT 'none';
             ALTER TABLE `ai_opponents` ADD `arena_enabled` TINYINT(1) NOT NULL DEFAULT '0';
             ALTER TABLE `ai_opponents` ADD `is_patrol` TINYINT(1) NOT NULL DEFAULT '0';
             ALTER TABLE `ai_opponents` ADD `avatar_link` VARCHAR(100) NOT NULL;
