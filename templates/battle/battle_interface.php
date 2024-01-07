1e161910ea81b2db9eb31e82202a3fc88f083e6f
Arthesia <129538454+aumu1031@users.noreply.github.com>
2024-01-07 13:34:13 -0500

    Spectate Display (#557)

--- templates/battle/battle_interface.php
+++ templates/battle/battle_interface.php
@@ -274,7 +274,15 @@ if($battle->battle_text) {
                         </label>
                         <div class='staminaFill' style='width:<?= $stamina_percent ?>%;'></div>
                     </div>
-
+                    <?php else: ?>
+                    <div class='resourceBarOuter chakraPreview' style='margin-top:6px;'>
+                        <label style="opacity: 75%" class='innerResourceBarLabel'>??? / ???</label>
+                        <div class='chakraFill' style='width:100%'></div>
+                    </div>
+                    <div class='resourceBarOuter staminaPreview' style='margin-top:6px;'>
+                        <label style="opacity: 75%" class='innerResourceBarLabel'>??? / ???</label>
+                        <div class='staminaFill' style='width:100%;'></div>
+                    </div>
                     <?php endif; ?>
                 </div>
             </div>
