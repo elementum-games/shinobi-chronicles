4f1a8de8c87fecdb7366fbcbae396ac7203d57d8
Kelly Baillie <anubiswork@gmail.com>
2024-01-14 00:36:44 +0000

    Fix

--- templates/admin/open_server.php
+++ templates/admin/open_server.php
@@ -3,7 +3,7 @@
         <tr><th>Open SC</th></tr>
         <tr>
             <td>
-                <form action="$self_link" method='post'>
+                <form action="<?=$self_link?>" method='post'>
                     Are you sure you would like to reopen SC?<br />
                     <input type='submit' name='open_sc' value='open' />
                 </form>
