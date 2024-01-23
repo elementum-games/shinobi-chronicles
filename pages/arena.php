3b3f5586bbccb79f737acc9ae20d08fd8085efb0
Audrey Mueller <audrey.mueller2@gmail.com>
2024-01-23 00:26:07 -0500

    tweak

--- pages/arena.php
+++ pages/arena.php
@@ -179,9 +179,9 @@ function processArenaBattleEnd(BattleManager|BattleManagerV2 $battle, User $play
             $stat_to_gain = $player->getTrainingStatForArena();
 
             $stat_gain_display = '<br />During the fight you realized a way to use your ' . System::unSlug($stat_to_gain) . ' a little
-            more effectively.';
+            more effectively.<br />';
             $stat_gain = TrainingManager::getAIStatGain($opponent->difficulty_level, $player->rank_num);
-            $stat_gain_display .= '<br />' . $player->addStatGain($stat_to_gain, $stat_gain) . '.';
+            $stat_gain_display .= $player->addStatGain($stat_to_gain, $stat_gain) . '.';
         }
 
         // Village Rep Gains
@@ -290,7 +290,7 @@ function processArenaBattleEnd(BattleManager|BattleManagerV2 $battle, User $play
             more effectively.<br />';
             $stat_gain = TrainingManager::getAIStatGain($opponent->difficulty_level, $player->rank_num);
             $stat_gain = floor($stat_gain / 2);
-            $stat_gain_display .= '<br />' . $player->addStatGain($stat_to_gain, $stat_gain) . '.';
+            $stat_gain_display .= $player->addStatGain($stat_to_gain, $stat_gain) . '.';
         }
 
         // Village Rep Gains
