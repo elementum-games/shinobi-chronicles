1da8e9358cc91081b47d28ebf6d74f708ae9cca7
Kelly Baillie <anubiswork@gmail.com>
2024-01-28 11:56:21 -0800

    Fix chat ban displays (#601)
    Fix system breaking variables
    
    Co-authored-by: Kelly Baillie <kbaillieworks@gmail.com>

--- secure/vars.sample.php
+++ secure/vars.sample.php
@@ -1,26 +1,20 @@
 <?php
-
-/*
-Make a copy of this as vars.php and change values to the ones for your environment. Then
-manually upload to your server, outside of git.
- */
-
-// This may be called outside the index, System is required for some of the default settings
+// This is used in other places, System is a requirement
 require_once __DIR__ . '/../classes/System.php';
 
 $ENVIRONMENT = System::ENVIRONMENT_DEV; // Use only ENVIRONMENT_DEV or ENVIRONMENT_PROD
-$ENABLE_DEV_ONLY_FEATURES = System::DEV_ONLY_FEATURES_DEFAULT; // Boolean value
+$ENABLE_DEV_ONLY_FEATURES = true; // Boolean value
 $LOCAL_HOST_CONNECTION = System::LOCAL_HOST; // Boolean value, set to false for running on websites
-$REGISTER_OPEN = true;
+$register_open = true;
 $SC_OPEN = true;
 $USE_NEW_BATTLES = false;
 $WAR_ENABLED = true;
 $REQUIRE_USER_VERIFICATION = false;
 
-$WEB_URL = 'http://localhost/';
+$web_url = 'http://localhost/';
 
 //Host needs to match the DB's host. For docker setup, rename it to "shinobi-chronicles-db".
 $host = 'localhost';
-$username = 'database_username';
-$password = 'database_password';
-$database = 'database_name';
+$username = 'sc_admin';
+$password = 'password';
+$database = 'shinobi_chronicles';
