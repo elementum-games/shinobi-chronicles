2901d8ff3cca991469b8cf7c679da8b28e89d36b
Arthesia <129538454+aumu1031@users.noreply.github.com>
2024-02-07 17:57:55 -0500

    Modal Component (#616)
    * Modal Component
    
    * tweak

---
+++ ui_components/src/utils/modal.js
@@ -0,0 +1,24 @@
+export function Modal({ isOpen, header, text, ContentComponent, componentProps, onConfirm, onClose }) {
+    return (
+        <>
+            {isOpen && "closed" && (
+                <>
+                    <div className="modal_backdrop"></div>
+                    <div className="modal">
+                        <div className="modal_header">{header}</div>
+                        <div className="modal_text">{text}</div>
+                        {ContentComponent && (
+                            <div className="modal_content">
+                                <ContentComponent {...componentProps} />
+                            </div>
+                        )}
+                        {onConfirm && ( 
+                            <div className="modal_confirm_button" onClick={() => onConfirm()}>confirm</div>
+                        )}
+                        <div className="modal_cancel_button" onClick={() => onClose()}>cancel</div>
+                    </div>
+                </>
+            )}
+        </>
+    );
+}
---
+++ ui_components/src/utils/modalContext.js
@@ -0,0 +1,35 @@
+import { Modal } from './modal.js';
+
+const ModalContext = React.createContext();
+export const useModal = () => React.useContext(ModalContext);
+
+export const ModalProvider = ({ children }) => {
+    const [modalProps, setModalProps] = React.useState({
+        isOpen: false,
+        header: '',
+        text: '',
+        ContentComponent: null,
+    });
+
+    const openModal = ({ header, text, ContentComponent, componentProps, onConfirm }) => {
+        setModalProps({
+            isOpen: true,
+            header,
+            text,
+            ContentComponent,
+            componentProps,
+            onConfirm,
+        });
+    };
+
+    const closeModal = () => {
+        setModalProps((prevProps) => ({ ...prevProps, isOpen: false }));
+    };
+
+    return (
+        <ModalContext.Provider value={{ ...modalProps, openModal, closeModal }}>
+            {children}
+            <Modal {...modalProps} onClose={closeModal} />
+        </ModalContext.Provider>
+    );
+};
