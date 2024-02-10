33445b3afe9c1ddc773fb254667a1eb1fbab01cf
Audrey Mueller <audrey.mueller2@gmail.com>
2024-02-09 19:45:33 -0500

    village upgrades UI initial commit

--- classes/village/VillageBuildingDto.php
+++ classes/village/VillageBuildingDto.php
@@ -9,6 +9,13 @@ class VillageBuildingDto {
         public int $health = 0,
         public string $status = VillageBuildingConfig::BUILDING_STATUS_DEFAULT,
         public ?int $construction_progress = null,
-        public ?int $construction_progress_required = null
+        public ?int $construction_progress_required = null,
+        // display-only properties
+        public string $name = "",
+        public ?int $materials_construction_cost = null,
+        public ?int $food_construction_cost = null,
+        public ?int $wealth_construction_cost = null,
+        public ?int $construction_time = null,
+        public array $upgrade_sets = [],
     ) {}
 }
