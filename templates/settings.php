c324efd7bf80e1483b5c8ea2d1e35a6916958460
Kelly Baillie <anubiswork@gmail.com>
2024-02-24 15:10:13 -0800

    Update settings.php
    Missing line break

--- templates/settings.php
+++ templates/settings.php
@@ -76,7 +76,7 @@
                     <?php if(!$player->forbidden_seal->direct_avatar_upload): ?>
                         Avatar must be hosted on another website<br />
                     <?php else: ?>
-                        <b>WARNING:</b> You can only upload a file once per day!
+                        <b>WARNING:</b> You can only upload a file once per day!<br />
                     <?php endif ?>
                     Default limit: <?=$player->getAvatarSize()?> x <?=$player->getAvatarSize()?> pixels<br />
                     Avatar can be larger than the limit, but it will be resized<br />
